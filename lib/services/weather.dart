95c77ab5beec2f02aeda6b7f6650f07e877b8182
KaanOzen-MF <kaanozn98@gmail.com>
2021-10-17 16:18:13 +0300

    close to end

--- lib/services/weather.dart
+++ lib/services/weather.dart
@@ -1,4 +1,23 @@
+import 'package:clima/services/location.dart';
+import 'package:clima/services/networking.dart';
+
+const apiKey = "36b72a65c576cc41697dd1d8b8305ca2";
+const apiURL = "https://api.openweathermap.org/data/2.5/weather";
 class WeatherModel {
+
+  Future<dynamic> getLocationWeather() async{
+
+    Location location = Location();
+    await location.getCurrentLocation();
+
+    NetworkHelper networkHelper = NetworkHelper("$apiURL?lat=${location.latitude}"
+        "&lon=${location.longitude}&appid=$apiKey&units=metric");
+
+    var weatherData = await networkHelper.getData();
+    return weatherData;
+  }
+
+
   String getWeatherIcon(int condition) {
     if (condition < 300) {
       return '🌩';
